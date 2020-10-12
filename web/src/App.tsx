4b732b6b57a0130dd510cdc941dc52c33d7cf9b7
NetoJL <zeneto2604@gmail.com>
2020-10-12 14:27:57 -0300

    NLW#3 Dia01

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FiArrowDownRight, FiArrowRight} from 'react-icons/fi'
+
+import './styles/global.css'
+
+import Routes from './routes';
+
+function App() {
+  return (
+  <Routes />    
+  );
+}
+
+export default App;
